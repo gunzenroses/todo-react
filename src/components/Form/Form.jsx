ad074237e6c573598830865ee4e1b457c045a42c
Karina G <coren.gun@gmail.com>
2021-07-22 13:44:42 +0800

    feat: code for adding a new note has been moved to a separate function

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -17,21 +17,23 @@ function Form(props){
         })
     }
 
+    function hadnleAddition(event){
+        event.preventDefault();
+        if (note.title !== "" && note.text !== ""){
+            props.onAddition(note);
+            setNote({title: "", text: ""});
+            document.getElementsByName("title")[0].value = "";
+            document.getElementsByName("text")[0].value = "";
+        }
+    }
+
     return (
         <div className="form" onChange= { handleChange }>
             <input className="form_title" name="title" placeholder="Title"/>
             <textarea className="form__text" name="text" placeholder="Describe the task..."></textarea>
             <button 
                 className="form__submit" 
-                onClick={()=>{
-                            if (note.title !== "" && note.text !== ""){
-                                props.onAddition(note);
-                                setNote({title: "", text: ""});
-                                document.getElementsByName("title")[0].value = "";
-                                document.getElementsByName("text")[0].value = "";
-                            }
-                    }
-                }>Add to list
+                onClick={ hadnleAddition }>Add to list
             </button>
         </div>
     )
